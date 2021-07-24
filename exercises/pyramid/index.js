9dd47c875f65e4dc87257c5936bcea6fa9425e0d
Sandip <sandipramanik07@gmail.com>
2021-07-24 20:37:35 +0530

    Add solution for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,40 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, i = 0, level = "") {
+  // *1
+  // const midpoint = Math.floor((2 * n - 1) / 2);
+  // for (let i = 0; i < n; i++) {
+  //   let level = "";
+  //   for (let j = 0; j < 2 * n - 1; j++) {
+  //     if (j >= midpoint - i && j <= midpoint + i) {
+  //       level += "#";
+  //     } else {
+  //       level += " ";
+  //     }
+  //   }
+  //   console.log(level);
+  // }
+
+  // *2
+  const midpoint = Math.floor((2 * n - 1) / 2);
+
+  if (n === i) {
+    return;
+  }
+
+  if (2 * n - 1 === level.length) {
+    console.log(level);
+    return pyramid(n, i + 1);
+  }
+
+  if (level.length >= midpoint - i && level.length <= midpoint + i) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+
+  return pyramid(n, i, level);
+}
 
 module.exports = pyramid;
