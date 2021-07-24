ee80feab6588209918d6af412dea6808c854a9a7
Sandip <sandipramanik07@gmail.com>
2021-07-24 21:16:09 +0530

    Add solution for vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str, n = 0, count = 0) {
+  // *1
+  // if (n === str.length) {
+  //   return count;
+  // }
+  // if (str[n].toLowerCase().match(/[aeiou]/)) count++;
+  // return vowels(str, n + 1, count);
+
+  // *2
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
