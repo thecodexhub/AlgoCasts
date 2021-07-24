23d0f5a0a3ef030be9e51c3ed7534aba044d3bcc
Sandip <sandipramanik07@gmail.com>
2021-07-24 16:44:09 +0530

    Add solutions to steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,37 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = "") {
+  // *1
+  // for (let i = 0; i < n; i++) {
+  //   let stair = "";
+  //   for (let j = 0; j < n; j++) {
+  //     if (j <= i) {
+  //       stair += "#";
+  //     } else {
+  //       stair += " ";
+  //     }
+  //   }
+  //   console.log(stair);
+  // }
+
+  // *2
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+
+  return steps(n, row, stair);
+}
 
 module.exports = steps;
